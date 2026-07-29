6e472502b0dc47f7c6fb78d2b02a516000d3c027
F1ST <Konrad03Nowak@gmail.com>
2026-07-29 09:26:57 +0200

    Silence cmake warning, use propper preprocessor guard

--- vkpp/error/vk_error_config.hpp
+++ vkpp/error/vk_error_config.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#if __cpp_impl_reflection // NOLINT
+#if defined(__cpp_impl_reflection) && __cpp_impl_reflection
 #define UTILS_VK(expr, fn) ::vkpp::map_vk_error<fn>(expr)
 #else
 #define UTILS_VK(expr, fn) ::vkpp::map_vk_error(expr)
